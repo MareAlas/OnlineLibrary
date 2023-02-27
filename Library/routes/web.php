33dc0d781a483d2e935b879eb76c4b3c3a2310b4
MareAlas <markovicm.1403@gmail.com>
2023-02-28 00:24:01 +0100

    Add middleware
    Add Middleware.
    Change redirect.
    Using middleware in routing

--- Library/routes/web.php
+++ Library/routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('librarian')->group(function(){
+Route::prefix('librarian')->middleware(['auth', 'isLibrarian'])->group(function(){
     Route::get('/authors', [App\Http\Controllers\Librarian\AuthorController::class, 'index'])->name('index');
 });
 
