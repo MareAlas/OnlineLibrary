0afd348ef41165538159b67f2ef053b7b9061b0f
MareAlas <markovicm.1403@gmail.com>
2023-02-27 22:34:33 +0100

    Create 2023_02_27_213057_add_details_to_users_table.php
    Add role_as (0=reader, 1=librarian).
    Add surname in users table.
    Migration.

---
+++ Library/database/migrations/2023_02_27_213057_add_details_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('role_as')->default('0')->comment('0=reader, 1=librarian');
+            $table->string('surname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_as');
+        });
+    }
+};
