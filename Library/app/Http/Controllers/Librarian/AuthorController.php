bd547ee168cd346457d5bd25ae5f6b30c1f8ffb9
MareAlas <markovicm.1403@gmail.com>
2023-02-28 15:38:49 +0100

    Store function
    Add Author into database.
    Usig store unction and form request validation

--- Library/app/Http/Controllers/Librarian/AuthorController.php
+++ Library/app/Http/Controllers/Librarian/AuthorController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Librarian;
 
-use App\Http\Controllers\Controller;
+use App\Models\Author;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthorFromRequest;
 
 class AuthorController extends Controller
 {
@@ -16,4 +18,26 @@ class AuthorController extends Controller
     {
         return view('librarian.authors.create');
     }
+
+    public function store(AuthorFromRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $author = new Author;
+        $author->name               = $validatedData['name'];
+        $author->surname            = $validatedData['surname'];
+
+        $uploadPath                 = 'uploads/authors/';
+        if($request->hasFile('image'))
+        {
+            $file                   = $request->file('image');
+            $ext                    = $file->getClientOriginalExtension();
+            $filename               = time().'.'.$ext;
+            $file->move('uploads/authors/', $filename);
+            $author->image          = $uploadPath.$filename;           
+        }
+        $author->save();
+        return redirect('authors/index')->with('message', 'Author created successufully');
+    }
+
 }
