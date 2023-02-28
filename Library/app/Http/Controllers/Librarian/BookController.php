91f0ee4d872db6a78753598fdf4cfd596e782d56
MareAlas <markovicm.1403@gmail.com>
2023-02-28 21:59:28 +0100

    Show books on books.index
    Show books on books.index

--- Library/app/Http/Controllers/Librarian/BookController.php
+++ Library/app/Http/Controllers/Librarian/BookController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Librarian;
 
+use App\Models\Book;
 use App\Models\Author;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookFormRequest;
 
 class BookController extends Controller
 {
     public function index()
     {
-        return view('librarian.books.index');
+        $books = Book::all();
+        return view('librarian.books.index', compact('books'));
     }
 
     public function create()
--- Library/resources/views/librarian/books/index.blade.php
+++ Library/resources/views/librarian/books/index.blade.php
@@ -19,7 +19,22 @@
                     </tr>
                 </thead>
                 <tbody>
-            
+                    @forelse ($books as $booksItem)
+                    <tr>
+                        <td>{{ $booksItem->id }}</td>
+                        <td>{{ $booksItem->title }}</td>
+                        <td>{{ $booksItem->description }}</td>
+                        <td>{{ $booksItem->book_number }}</td>
+                        <td>
+                            <a href="{{ url('librarian/books/'.$booksItem->id.'/edit') }}" class="btn btn-success">Edit</a>
+                            <a href="{{ url('librarian/books/'.$booksItem->id.'/delete') }}" 
+                                onclick="return confirm('Are you sure want to delete this data?')" 
+                                class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr> 
+                    @empty
+                        No Books  
+                    @endforelse
                 </tbody>
             </table>
         </div>
