9b47510ac024ba5115fd2afffd2e5b520ffe263e
MareAlas <markovicm.1403@gmail.com>
2023-03-01 21:33:39 +0100

    librarian start page
    librarian start page

--- Library/resources/views/librarian/dashboard.blade.php
+++ Library/resources/views/librarian/dashboard.blade.php
@@ -2,9 +2,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-md-12">
-        Welcome Librarian
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <p style="text-align: center;">Welcome to Librarian Page</p> 
+            </div>
+        </div> 
     </div>
 </div> 
 @endsection
