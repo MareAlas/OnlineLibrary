182eca5c584f2a0e723510b9e198681846968f52
MareAlas <markovicm.1403@gmail.com>
2023-03-01 21:46:42 +0100

    Show all books - reader
    Show all books for reader

--- Library/resources/views/home.blade.php
+++ Library/resources/views/home.blade.php
@@ -18,6 +18,29 @@
                         <label>Search book</label>
                         <input type="search" name="search" id="search" value="" class="form-control" placeholder="Search"/>
                     </div>
+                    <div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Book Title</th>
+                                    <th>Book Number</th>
+                                    <th>Author</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($books as $book)
+                                <tr>
+                                    <td>{{ $book->title }}</td>
+                                    <th>{{ $book->book_number }}</th>
+                                    <th>{{ $book->author_id }}</th>
+                                </tr>
+                                @empty
+                                    No Books
+                                @endforelse
+                               
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
            
             </div>
